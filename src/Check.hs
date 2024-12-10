3945b07f25bc79ce65209de3a1498c728315e400
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-12-09 20:16:04 -0500

    make module and bindings checkable

--- src/Check.hs
+++ src/Check.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Check (Checkable (..), checkExpr) where
+module Check (checkExpr) where
 
-import AST (Binder (binderName), Expr (..), OpChain (..))
+import AST (Binder (..), Binding (..), Expr (..), Module (..), OpChain (..))
 import Control.Monad.Trans.Reader (Reader, ask, local, runReader)
 import Data.Set (Set)
 import qualified Data.Set as S
@@ -34,10 +35,26 @@ instance Checkable Expr where
   check (Fun params body _) = do
     let params' = S.fromList (map binderName params)
     let paramErrors = ["duplicate params" | length params /= S.size params']
-    bodyErrors <- local (S.union params') (check body)
-    return (paramErrors ++ bodyErrors)
+    (paramErrors ++) <$> local (S.union params') (check body)
   check (Parens expr _) = check expr
   check (Chain chain) = check chain
+
+instance Checkable Binding where
+  check :: Binding -> Reader Ctx [Text]
+  check (Binding (Binder name _) _ expr _) = do
+    names <- ask
+    let binderErrors = [name <> " already exists" | S.member name names]
+    (binderErrors ++) <$> local (S.insert name) (check expr)
+
+instance Checkable [Binding] where
+  check :: [Binding] -> Reader Ctx [Text]
+  check [] = return []
+  check (b@(Binding (Binder name _) _ _ _) : bs) =
+    (++) <$> check b <*> local (S.insert name) (check bs)
+
+instance Checkable Module where
+  check :: Module -> Reader Ctx [Text]
+  check (Module bs) = check bs
 
 checkExpr :: Expr -> Either [Text] ()
 checkExpr expr =
