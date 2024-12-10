f6ec0d0eb130c7ef4b4f84ff2e11628754e162cc
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-12-09 19:29:22 -0500

    make expr and operation chains checkable

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Check (check)
+import Check (checkExpr)
 import Control.Monad (forM_, guard)
 import Data.Either (fromLeft)
 import Data.Text.IO as TIO (putStrLn, readFile)
@@ -15,6 +15,6 @@ main = do
   let filePath = head args
   code <- TIO.readFile filePath
   let expr = parseTokens $ lexText code
-  let errors = fromLeft [] (check expr)
+  let errors = fromLeft [] (checkExpr expr)
   forM_ errors TIO.putStrLn
   print expr
--- src/Check.hs
+++ src/Check.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Check (check) where
+module Check (Checkable (..), checkExpr) where
 
 import AST (Binder (binderName), Expr (..), OpChain (..))
 import Control.Monad.Trans.Reader (Reader, ask, local, runReader)
@@ -10,30 +11,35 @@ import Data.Text (Text)
 
 type Ctx = Set Text
 
-checkChain :: OpChain -> Reader Ctx [Text]
-checkChain (Operand expr) = check' expr
-checkChain (Operation chain op l) =
-  concat <$> sequence [checkChain chain, check' op, check' l]
+class Checkable t where
+  check :: t -> Reader Ctx [Text]
 
-check' :: Expr -> Reader Ctx [Text]
-check' (Int _ _) = return []
-check' (Float _ _) = return []
-check' (Id name _) = do
-  names <- ask
-  if S.member name names
-    then return []
-    else return [name <> " is not defined"]
-check' (App f args _) =
-  concat <$> mapM check' (f : args)
-check' (Fun params body _) = do
-  let params' = S.fromList (map binderName params)
-  let paramErrors = ["duplicate params" | length params /= S.size params']
-  bodyErrors <- local (S.union params') (check' body)
-  return (paramErrors ++ bodyErrors)
-check' (Parens expr _) = check' expr
-check' (Chain chain) = checkChain chain
+instance Checkable OpChain where
+  check :: OpChain -> Reader Ctx [Text]
+  check (Operand expr) = check expr
+  check (Operation chain op l) =
+    concat <$> sequence [check chain, check op, check l]
 
-check :: Expr -> Either [Text] ()
-check expr =
-  let errors = runReader (check' expr) S.empty
+instance Checkable Expr where
+  check :: Expr -> Reader Ctx [Text]
+  check (Int _ _) = return []
+  check (Float _ _) = return []
+  check (Id name _) = do
+    names <- ask
+    if S.member name names
+      then return []
+      else return [name <> " is not defined"]
+  check (App f args _) =
+    concat <$> mapM check (f : args)
+  check (Fun params body _) = do
+    let params' = S.fromList (map binderName params)
+    let paramErrors = ["duplicate params" | length params /= S.size params']
+    bodyErrors <- local (S.union params') (check body)
+    return (paramErrors ++ bodyErrors)
+  check (Parens expr _) = check expr
+  check (Chain chain) = check chain
+
+checkExpr :: Expr -> Either [Text] ()
+checkExpr expr =
+  let errors = runReader (check expr) S.empty
    in if null errors then Right () else Left errors
