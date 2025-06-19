f6e1019c3a0ae3238c5c6673107ae3dfebfb7d4b
Eduardo García Maleta <egmaleta.secondary@proton.me>
2025-06-19 18:45:43 -0400

    remove happy rules file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 alex src/Fine/Lexer.x -o src/Fine/Lexer.hs --ghc
-happy src/Fine/Parser.y -o src/Fine/Parser.hs --ghc --info=happy-rules.txt
+happy src/Fine/Parser.y -o src/Fine/Parser.hs --ghc
 cabal run fine-compiler -- $1 $2
